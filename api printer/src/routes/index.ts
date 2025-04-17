c12e1738066354957dcab5397aa7627c753dd22f
nguyenlocDev <136955275+nguyenlocDev@users.noreply.github.com>
2025-04-17 07:13:38 +0700

    Delete api printer directory

--- api printer/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { circlekRoute } from "./circlek.route";
-
-const Router = express.Router();
-
-Router.use("/circlek", circlekRoute);
-
-export const API = Router;
